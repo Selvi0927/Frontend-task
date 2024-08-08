9cdc86e8e038d13ad5432590a552b8008e889b11
balaselvi0927 <balaselvi0927@gmail.com>
2024-08-08 07:08:11 +0000

    project

---
+++ src/pages/project/index.tsx
@@ -0,0 +1,10 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2694603518.
+import React from "react";
+const Home=()=>{
+    return (
+     <div>
+   welcome the project
+      </div>
+    );
+  }
+  export default Home;
